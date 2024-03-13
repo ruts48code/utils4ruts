fca74542710900074a92137f01ba4792effc311d
Authapon Kongwan <authapon@gmail.com>
2024-03-13 15:07:15 +0700

    start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ruts48code/utils4ruts
+
+go 1.22.1
+
+require github.com/ruts48code/random4ruts v0.0.0-20240313075618-4dea7e44a30a // indirect
---
+++ main.go
@@ -0,0 +1,52 @@
+package utils4ruts
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	random "github.com/ruts48code/random4ruts"
+)
+
+func ReadFile(f string) []byte {
+	content, err := os.ReadFile(f)
+	if err != nil {
+		return []byte("")
+	}
+	return content
+}
+
+func MakeString(s string) string {
+	return fmt.Sprintf("%s", s)
+}
+
+func RandomArrayString(data []string) []string {
+	output := make([]string, 0)
+	input := make([]string, len(data))
+	copy(input, data)
+	for {
+		if len(input) == 0 {
+			return output
+		}
+		choose := int(random.GetRandomInt(int64(len(input))))
+		output = append(output, input[choose])
+		input = DeleteArrayString(input, choose)
+	}
+}
+
+func DeleteArrayString(data []string, n int) []string {
+	return append(data[:n], data[n+1:]...)
+}
+
+func GetTimeStamp(t time.Time) string {
+	return t.Format("2006-01-02T15:04:05")
+}
+
+func GetDateStamp(t time.Time) string {
+	return t.Format("2006-01-02")
+}
+
+func NormalizeUsername(username string) string {
+	return strings.TrimSpace(strings.ToLower(username))
+}
